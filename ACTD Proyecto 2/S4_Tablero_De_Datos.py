090deb11d355f60b7445bb0bf69836acd25973b5
luisav21 <l.velezv@uniandes.edu.co>
2024-05-01 19:40:23 -0500

    Update S4_Tablero_De_Datos.py
    Se adjunta el logo de la universidad

--- ACTD Proyecto 2/S4_Tablero_De_Datos.py
+++ ACTD Proyecto 2/S4_Tablero_De_Datos.py
@@ -8,3 +8,6 @@ data = pd.read_csv("datosproyecto2")
 
 #Crear la Dash app
 app = dash.Dash(__name__)
+
+# Logo de la Universidad de Los Andes :)
+logo_url = "https://images.ctfassets.net/wp1lcwdav1p1/32ZvbT2qtVDItdoxBhRHRf/ee5581294ae18385bf17cbccdcd74a79/LOGOS_Ingenieri__a_Uniandes_2018-_Color.png?q=60"
